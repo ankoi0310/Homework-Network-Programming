a7567b07b7988bfd70a262dd07a84a56c59f4d3b
Huynh Van Huu An <19130003@st.hcmuaf.edu.vn>
2021-10-20 12:32:27 +0700

    Update Exercise 8

--- src/IO/Exercise8.java
+++ src/IO/Exercise8.java
@@ -3,35 +3,53 @@ package IO;
 import java.io.*;
 
 public class Exercise8 {
-
     public static boolean fileCopy(String sFile, String destFile, boolean moved) {
         File file = new File(sFile);
-        if (file.exists() && file.isFile()) {
-            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(sFile));
-                 BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(destFile))) {
-                int length;
-                long start = System.currentTimeMillis();
-                while ((length = bis.read()) != -1) {
-                    bos.write(length);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                throw new RuntimeException("This is not a file");
+            }
+            if (file.isFile()) {
+                try (FileInputStream fis = new FileInputStream(sFile);
+                     BufferedInputStream bis = new BufferedInputStream(fis);
+                     FileOutputStream fos = new FileOutputStream(destFile);
+                     BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    long start = System.currentTimeMillis();
+                    while ((length = bis.read(buffer)) != -1) {
+                        bos.write(buffer, 0, length);
+                    }
+                    System.out.println("Copy/move time: " + (System.currentTimeMillis() - start) + "ms");
+                    bis.close();
+                    bos.close();
+                    fis.close();
+                    fos.close();
+                    if (moved) {
+                        boolean deleted = file.delete();
+                        if (deleted) {
+                            return true;
+                        } else {
+                            File dest = new File(destFile);
+                            if (dest.exists()) {
+                                dest.delete();
+                                return false;
+                            }
+                        }
+                    }
+                    return true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
                 }
-                System.out.println("Copy/move time: " + (System.currentTimeMillis() - start) + "ms");
-                bis.close();
-                bos.close();
-                if (moved) {
-                    file.delete();
-                }
-                return true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
             }
         }
         return false;
     }
 
     public static void main(String[] args) {
-        String sFile = "D:\\NLU\\STUDY\\2021 - 2022\\Semester 1\\Network Programming\\Test - Copy";
-        String destFile = "D:\\NLU\\STUDY\\2021 - 2022\\Semester 1\\Network Programming\\Test - Copy1";
-        System.out.println(fileCopy(sFile, destFile, false) ? "Successfully" : "Fail");
+        String sFile = "D:\\NLU\\STUDY\\2021 - 2022\\Semester 1\\Network Programming\\Lab\\Net-Program-Exercise.docx";
+        String destFile = "D:\\NLU\\STUDY\\2021 - 2022\\Semester 1\\Network Programming\\Lab\\Net-Program-Exercise Copy.docx";
+        System.out.println(fileCopy(sFile, destFile, true) ? "Successfully" : "Fail");
     }
 }
